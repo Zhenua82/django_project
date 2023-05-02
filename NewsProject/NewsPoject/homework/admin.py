6fb28d358c5f55aabc64260005131bef5661db44
Zhenua82 <zzhmenka@yandex.ru>
2023-05-02 18:16:21 +0300

    add admin

--- NewsProject/NewsPoject/homework/admin.py
+++ NewsProject/NewsPoject/homework/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Human
+
+class HumanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Name', 'Last_name', 'age', 'profession', 'photo', 'is_published')
+    list_display_links = ('id', 'Name')
+    search_fields = ('Name', 'Last_name')
+
+
+admin.site.register(Human, HumanAdmin)
+
 
 # Register your models here.
